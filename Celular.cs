322993f23a272fe809d0b9e13a1f068327ce0191
Tania De Oliveira Lima Dos Santos <1185849874@info.local>
2023-04-25 11:08:02 -0300

    aula 24.04

---
+++ Celular.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace celular
+{
+    public class Celular
+    {
+        public string marca;
+        public string modelo;
+        public string cor;
+        public string tamanho;
+        public boll Ligado;
+        
+    //proximo passo devemos declarar os metodos para a classe celular
+//metodo usa um verbo de acao entao; ligar, desligar, fazer ligação, enviar mensagem
+
+ligar
+desligar
+fazer ligacao
+enviar mensagem
+
+{
+    if Celular ligado
+    {
+       faz ligacao
+       envia mensagem
+    }
+    else
+    {
+       desligado 
+    }
+}
+
+// so executa metodo quando ligado
+
+System.Console.WriteLine($"Informe a marca do Celular");
+marcaCelular.marca = Console.ReadLine();
+
+System.Console.WriteLine($"Informe o modelo do Celular");
+modeloCelular.modelo = Console.ReadLine();      
+ 
+System.Console.WriteLine($"Informe a cor do Celular");
+corCelular.cor = Console.ReadLine(); 
+ 
+System.Console.WriteLine($"Informe o tamanho do Celular");
+tamanhoCelular.tamanho = Console.ReadLine();
+    }
+}
